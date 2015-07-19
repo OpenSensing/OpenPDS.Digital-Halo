1975581eebef5c5a419c65b2c1683480e6b4ff9c
yellowBirdy <mieszkomieszko@gmail.com>
2015-07-19 15:08:39 +0200

    Added a dummy PDS server logging the urls and made the extension to call it

--- data_collection/lib/main.js
+++ data_collection/lib/main.js
@@ -4,12 +4,21 @@ var Request = require('sdk/request').Request;
 
 tabs.on('ready', alertURL);
 
+
 function alertURL (tab) {
     tab.attach  ({
-        contentScript: 'window.alert("Page matches ruleset");'
+        contentScript: 'window.alert("Page url extracted ");'
         //contentScript: 'document.body.innerHTML = "Page matches ruleset"'
     });
-    console.log(tabs.activeTab.url)
+    console.log(tabs.activeTab.url);
+    Request({ url: "http://localhost:8000/",
+	contentType: 'application/json',
+	content: JSON.stringify({'sentUrl' : tabs.activeTab.url}),
+        onComplete: function (res) {
+	console.log(res)
+        }
+    }).post()
+
 };
 
 // a dummy function, to show how tests work.
--- dummy_pds/dummy-pds.js
+++ dummy_pds/dummy-pds.js
@@ -1,16 +1,22 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var http = require('http')
+  , url = require('url')
+  , fs = require('fs')
+  , express = require('express')
+  , bodyParser = require('body-parser')
+  , pds = express()
 
-var server = http.createServer(saveData)
+pds.use(bodyParser.json());
+//pds.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//      extended: true
+//}));
+
 function saveData (req, res) {
-    if (req.method !== "GET") return console.log("terminatin - not a GET request")
-    
-    
-    fs.appendFileSync("./data/url-log.txt", req.url + '\n');
-    console.log('requeted URL logged: ' + req.url)
-    res.write('<h1>url saved<h1>')
-    
+    fs.appendFileSync("./data/url-log.txt", "POST "+ req.body.sentUrl + '\n');
+    console.log('requested URL logged: ' + req.body.sentUrl)
+    res.send('<h1>url saved<h1>')
 }
 
-server.listen(8000)
+pds.get("/:id", function (req,res) {res.send({answer : "What do you want?"})});
+pds.post("/", saveData)
+
+pds.listen(8000)
