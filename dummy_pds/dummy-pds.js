00343690ebee23415051138a659a181b365cf71a
yellowBirdy <mieszkomieszko@gmail.com>
2015-07-20 09:59:40 +0200

    Added mongo DB storage to dummy PDS

--- dummy_pds/dummy-pds.js
+++ dummy_pds/dummy-pds.js
@@ -3,7 +3,10 @@ var http = require('http')
   , fs = require('fs')
   , express = require('express')
   , bodyParser = require('body-parser')
-  , pds = express()
+  , MongoClient = require('mongodb').MongoClient;
+
+var pds = express()
+  , mongoUrl = 'mongodb://localhost:27017/dummy-pds';
 
 pds.use(bodyParser.json());
 //pds.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -11,9 +14,30 @@ pds.use(bodyParser.json());
 //}));
 
 function saveData (req, res) {
-    fs.appendFileSync("./data/url-log.txt", "POST "+ req.body.sentUrl + '\n');
-    console.log('requested URL logged: ' + req.body.sentUrl)
-    res.send('<h1>url saved<h1>')
+//    fs.appendFileSync("./data/url-log.txt", "POST "+ req.body.sentUrl + '\n');
+    MongoClient.connect(mongoUrl, function (err, db) {
+        if (err) return console.log('Unable to connect to mongo: '+ err)
+	console.log("Connected to mongo");
+        
+        insertToMongo(db, {userID: 1, url: req.body.sentUrl}, function () {
+            db.close()   
+	})
+    });
+
+    res.send('<h1>url saved<h1>');
+    
+}
+
+function insertToMongo (db, data, callback)  {
+    
+    var collection = db.collection('url');
+    collection.insert(
+      data, 
+      function (err, result) {
+          if (err) return console.log('Unable to insert to mongo: '+ err)
+          console.log('Inserted sent url: '+ data.url);
+	  callback(result)
+      });
 }
 
 pds.get("/:id", function (req,res) {res.send({answer : "What do you want?"})});
