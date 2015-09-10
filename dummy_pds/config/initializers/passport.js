dead06691235fc67fab9515920e4b7b4fe371b19
yellowBirdy <mieszkomieszko@gmail.com>
2015-09-10 19:06:00 +0200

    sorting out oidc auth strategy config

--- dummy_pds/config/initializers/passport.js
+++ dummy_pds/config/initializers/passport.js
@@ -4,25 +4,30 @@ function init() {
   var passport              = require('passport')
     , OpenIDConnectStrategy = require('passport-openidconnect').Strategy
     , PDSOpenIDConnectStrategy = new OpenIDConnectStrategy ({
-          authorizationURL: 'http://localhost:3000/oauth/authorize'
-        , tokenURL        : 'http://localhost:3000/oauth/token'
-        , clientID        : 'df4012ec3bffefd3e14cff0a4a734266'
-        , clientSecret    : '5d002be253376792bbbff204a9fccb36'
-        , callbackURL     : 'http://localhost:8000/auth/provider/callback'
-        , userInfoURL     : 'http://localhost:3000/oauth/userInfo'
+          authorizationURL: 'http://dev.sensible.dtu.dk:9091/oauth/authorize'
+        , tokenURL        : 'http://dev.sensible.dtu.dk:9091/oauth/token'
+        , clientID        : '80d57d3b95a32dbbe2e53a2e7e4c4325'
+        , clientSecret    : '03a5a086b396b6af84f21694d871110e'
+        , callbackURL     : 'http://dev.sensible.dtu.dk:9090/auth/provider/callback'
+        , userInfoURL     : 'http://dev.sensible.dtu.dk:9091/oauth/userInfo'
         , scope           : 'profile|email'
         },
       function (accessToken, refreshToken, profile, cb) {
         //process.nextTick(function() {
           App.model('user').findOne({'email': profile._json.email}, function (err, user) {
             if (err) return cb(err, null)
+            
             if (!user) {
               var u = new User({email: profile._json.email, passwordHash: null})
-              u.save(function (err){if (err) return console.log('While creating user ' + err)})
+              u.save(function (err, u){
+                if (err) return console.log('While creating user ' + err)
+              
+                return cb(null, u)
+              })
+            } else {
+	      cb(null, user)	  
             }
-
-	          cb(null, user)	  
-	        })  
+	  })  
 	      //})
       } 
     )
