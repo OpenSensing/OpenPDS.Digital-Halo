bf0db4d85e21fcfdca24a1708983f549fc657be6
Mieszko Manijak <mieszkomieszko@gmail.com>
2015-10-29 10:07:11 +0100

    Feature2 - handles recent history and trackers

--- chrome_extension/dropbox/index.js
+++ chrome_extension/dropbox/index.js
@@ -32,6 +32,43 @@ function readDemographics (e) {
     })
 }
 
+function sendRecent () {
+  // get the overall count of recorded visits
+  chrome.storage.local.get('recordedCount', function (items) {
+    // check if anything got recorded, if not then return
+    if (!items.recordedCount) return alert('No current history stored, nothing sent')
+
+    // get recent history file count to append to file name
+    readFromDropbox('config.json', function (config) {
+      config = deserialize(config)
+      config.raw_data.currentHistoryAndTrackers.file_count++
+      var fileName = 'currentHistory/currentHistoryAndTrackers' + (config.raw_data.currentHistoryAndTrackers.file_count) + '.json'
+       
+      var count = items.recordedCount;
+      // create pointer Array
+      var pagePointers = []
+      for (var i = 1; i <= count; i++) {
+        pagePointers.push('page' + i)
+      } 
+      // get the pages from storage and send them
+      chrome.storage.local.get(pagePointers, function (data) {
+        // merge the pages into a single json array
+        var payload = [];
+        for (page in data) {
+          payload.push(data[page])
+        } 
+        // send it
+        writeDropbox(payload, fileName)
+        console.log('sent recent history and trackers to Dropbox')
+        // clear the pages from local storage and reset the counter
+        chrome.storage.local.remove(pagePointers)
+        chrome.storage.local.set({'recordedCount': 0}) 
+      }) 
+      writeDropbox(config, 'config.json')
+    })
+  })
+}
+
 // register 
 window.addEventListener('load', function (e) {
   document.querySelector('#logIn').addEventListener('click', authenticateWithDropbox) 
@@ -39,4 +76,5 @@ window.addEventListener('load', function (e) {
   document.querySelector('#sendSDK').addEventListener('click', writeHistory)//writeDropbox)
   document.querySelector('#showAnswer').addEventListener('click', readDemographics)
   document.querySelector('nav').classList.add('move')
+  document.querySelector('#sendRecent').addEventListener('click', sendRecent)
 })
---
+++ chrome_extension/lib/background/background.js
@@ -0,0 +1,13 @@
+const MINUTE = 60000
+
+//store loaded page 
+chrome.runtime.onMessage.addListener(function(message, sender, cb) {
+  storeBrowsingAndTrackingLocaly(message, sender)
+})
+
+// send current browsing and tracking to dropbox
+setInterval(function () {
+  client.authenticate()
+  sendRecent() 
+
+}, 30 * MINUTE)
