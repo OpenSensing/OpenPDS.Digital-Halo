abf110b5d4408a8bd261f10f17e91faca94f6761
Mieszko Manijak <mieszkomieszko@gmail.com>
2015-11-01 19:22:39 +0100

    loading of history, recent updates domain counts

--- dropbox/process_data/raw_to_db.py
+++ dropbox/process_data/raw_to_db.py
@@ -1,4 +1,4 @@
-from db_utils import DB_PATH, RECENT_PATH
+from db_utils import DB_PATH, RECENT_PATH, APP_PATH
 import os
 import sqlite3
 import json
@@ -6,13 +6,39 @@ import json
 def recent_to_sqlite(file_handle):
 	content = json.loads(file_handle.read())
 	for el in content:
-		url_record = [el.get('sentUrl'), el.get('sentTitle') ,el.get('domain'), str(el.get('thirdPTrackers')), str(el.get('firstPTrackers')), el.get('accessedAt')]
+		# extract features
+		url        = el.get('sentUrl')
+		title      = el.get('sentTitle')
+		domain     = el.get('domain')
+		trackers   = el.get('thirdPTrackers')
+		if trackers : trackers = str(trackers)
+		otherXDRs  = el.get('firstPTrackers')
+		if otherXDRs: otherXDRs = str(otherXDRs)
+		accessedAt = el.get('accessedAt')
+
+		# URL table 
+		url_record = [url, title, domain, trackers, otherXDRs, accessedAt]
 		try: 
 			cur.execute('INSERT INTO url VALUES (?,?,?,?,?,?)', url_record)
 		except sqlite3.Error, e:
 			print 'error while inserting into url: ', e
-		#print el.get('sentTitle', '---- Title Missing ----')
+			print url_record
+
+		# DOMAINS table
+		insert_query = 'INSERT OR IGNORE INTO domain (name, total, recent) VALUES (?, ?, ?)'
+		update_query = 'UPDATE domain SET total=total+1,recent=recent+1 WHERE name="{0}"'.format( domain )
+		dom_record = [domain, ]
+		try:
+			cur.execute(insert_query, (domain, 0, 0))
+			cur.execute(update_query)
+		except sqlite3.Error, e:
+			print 'error while inserting into domain:',domain, e
+
+
+
 	db.commit()
+
+
 
 db  = sqlite3.connect(DB_PATH)
 cur = db.cursor()
--- dropbox/sqlit_init.py
+++ dropbox/sqlit_init.py
@@ -5,24 +5,25 @@ import json
 
 DB_PATH = APP_PATH + 'raw.db'
 
-print 'Initializatin of sqlite db: ',
-try:
-	create_tabs_queries = ["CREATE TABLE domain(name TEXT, total INT, count INT, ancient_count INT, domID INT, trackers TEXT, otherXDRs TEXT)",
-		"CREATE TABLE url(url TEXT, title TEXT, domain TEXT, trackers TEXT, otherXDRs TEXT, accessedAt INT)",
-		"CREATE TABLE tracker(name TEXT UNIQUE, domains TEXT, trackID INT)",
-		"CREATE TABLE otherXDR(name TEXT, domains TEXT, xdrID INT)"]
+def create_tables():
+	print 'Initializatin of sqlite db: ',
+	try:
+		create_tabs_queries = ["CREATE TABLE domain(name TEXT UNIQUE, total INT, recent INT, ancient_count INT, trackers TEXT, otherXDRs TEXT)",
+			"CREATE TABLE url(url TEXT, title TEXT, domain TEXT, trackers TEXT, otherXDRs TEXT, accessedAt INT)",
+			"CREATE TABLE tracker(name TEXT UNIQUE, domains TEXT, trackID INT)",
+			"CREATE TABLE otherXDR(name TEXT UNIQUE, domains TEXT, xdrID INT)"]
 
-	db  = sqlite3.connect(DB_PATH)
-	cur = db.cursor()
-	for query in create_tabs_queries: 
-		db.execute(query)
+		db  = sqlite3.connect(DB_PATH)
+		cur = db.cursor()
+		for query in create_tabs_queries: 
+			db.execute(query)
 
-	db.commit()
-	db.close()
+		db.commit()
+		db.close()
 
-	print 'Success'
-except sqlite3.Error, e:
-	print "Error %s:" % e.args[0]
+		print 'Success'
+	except sqlite3.Error, e:
+		print "Error %s:" % e.args[0]
 
 
-
+create_tables()
