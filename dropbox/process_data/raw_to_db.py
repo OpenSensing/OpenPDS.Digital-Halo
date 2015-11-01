931fc9e82faf633a85575929d8c66fc90d511acd
Mieszko Manijak <mieszkomieszko@gmail.com>
2015-11-01 22:00:06 +0100

    Added proper tracker and XDR handling to domain db

--- dropbox/process_data/raw_to_db.py
+++ dropbox/process_data/raw_to_db.py
@@ -2,6 +2,7 @@ from db_utils import DB_PATH, RECENT_PATH, APP_PATH
 import os
 import sqlite3
 import json
+import ast
 
 def recent_to_sqlite(file_handle):
 	content = json.loads(file_handle.read())
@@ -26,13 +27,33 @@ def recent_to_sqlite(file_handle):
 
 		# DOMAINS table
 		insert_query = 'INSERT OR IGNORE INTO domain (name, total, recent) VALUES (?, ?, ?)'
-		update_query = 'UPDATE domain SET total=total+1,recent=recent+1 WHERE name="{0}"'.format( domain )
-		dom_record = [domain, ]
+		update_counts_query = 'UPDATE domain SET total=total+1,recent=recent+1 WHERE name="{0}"'.format( domain )
+		
+		if trackers:
+			trackers = set(ast.literal_eval(trackers))
+		if otherXDRs:
+			otherXDRs = set(ast.literal_eval(otherXDRs))
+		
 		try:
 			cur.execute(insert_query, (domain, 0, 0))
-			cur.execute(update_query)
+			cur.execute(update_counts_query)
+			if trackers:
+				cur.execute('SELECT trackers FROM domain WHERE name="{0}"'.format(domain))
+				db_trackers = cur.fetchone()[0]
+				if db_trackers:
+					trackers = set(ast.literal_eval(db_trackers)).union(trackers) 
+				cur.execute('UPDATE domain SET trackers = "{0}" WHERE name="{1}"'.format(list(trackers), domain)) 
+			if otherXDRs:
+				cur.execute('SELECT otherXDRs FROM domain WHERE name="{0}"'.format(domain))
+				db_otherXDRs = cur.fetchone()[0]
+				if db_otherXDRs:
+					otherXDRs = set(ast.literal_eval(db_otherXDRs)).union(otherXDRs) 
+				cur.execute('UPDATE domain SET otherXDRs = "{0}" WHERE name="{1}"'.format(list(otherXDRs), domain)) 		
+
 		except sqlite3.Error, e:
 			print 'error while inserting into domain:',domain, e
+
+			print 'UPDATE domain SET trackers = "{0}" WHERE name="{1}"'.format(list(trackers), domain)
 
 
 
