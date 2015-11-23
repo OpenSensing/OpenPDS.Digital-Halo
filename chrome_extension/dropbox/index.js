aec5c6ba03896a4bad4100209955cf7dd7ab806e
Mieszko Manijak <mieszkomieszko@gmail.com>
2015-11-23 15:37:55 +0100

    materialize UI implemented

--- chrome_extension/dropbox/index.js
+++ chrome_extension/dropbox/index.js
@@ -75,11 +75,12 @@ function openPDS (e) {
 
 // register 
 window.addEventListener('load', function (e) {
-  document.querySelector('#logIn').addEventListener('click', authenticateWithDropbox) 
-  document.querySelector('#logOut').addEventListener('click', signOutOfDropbox)
-  document.querySelector('#sendSDK').addEventListener('click', writeHistory)//writeDropbox)
-  document.querySelector('#showAnswer').addEventListener('click', readDemographics)
+  document.querySelector('#log_in').addEventListener('click', authenticateWithDropbox) 
+  document.querySelector('#log_out').addEventListener('click', signOutOfDropbox)
+  //document.querySelector('#sendSDK').addEventListener('click', writeHistory)//writeDropbox)
+  //document.querySelector('#showAnswer').addEventListener('click', readDemographics)
   //document.querySelector('nav').classList.add('move')
-  document.querySelector('#sendRecent').addEventListener('click', sendRecent)
-  document.querySelector('#openPDS').addEventListener('click', openPDS)
+  //document.querySelector('#sendRecent').addEventListener('click', sendRecent)
+  document.querySelector('#meetMonsters').addEventListener('click', readDemographics)
+  document.querySelector('#openYourPDS').addEventListener('click', openPDS)
 })
