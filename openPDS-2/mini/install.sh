510b4fb3cd76d3c800ef63d49421147369af0ad0
Mieszko Manijak <mieszkomieszko@gmail.com>
2016-03-29 15:44:58 +0200

    draft of chrome native messanger host

--- openPDS-2/mini/install.sh
+++ openPDS-2/mini/install.sh
@@ -6,6 +6,11 @@ mkdir ${PDSPATH}
 mkdir ${PDSPATH}/parser
 mkdir ${PDSPATH}/answer_module
 
+
+#################### TODO  ##########
+# copy parser, the AM and any other needed stuff
+
+
 curl ${REPO}/mini/halo.py --output ${PDSPATH}/halo.py
 chmod 755 ${PDSPATH}/halo.py
 
@@ -14,16 +19,33 @@ curl ${REPO}/answer_module/digital_halo.py --output ${PDSPATH}/answer_module/dig
 
 curl ${REPO}/parser/manifest.json --output ${PDSPATH}/parser/manifest.json
 curl ${REPO}/answer_module/manifest.json --output ${PDSPATH}/answer_module/manifest.json
-#################### TODO  ##########
-# copy parser, the AM and any other needed stuff
-
 
 
 #################### TODO  ##########
 # edit the native messaging host and copy it to 
 # relevant chrome/chromium dir
+curl ${REPO}/chrome_native_messaging/dk.dtu.openpds.json --output dk.dtu.openpds.json
+# update the messaging host config with path to the pds executable
+perl -si -pe 's/LOCALPDSPATH/$pdsPath/' -- -pdsPath=${PDSPATH} dk.dtu.openpds.json
+#copy the file to the chrome/chromium messaging hosts folder
 
+if [ "$(uname -s)" == "Darwin" ]; then
+  if [ "$(whoami)" == "root" ]; then
+    MESSAGE_HOST_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+  else
+    MESSAGE_HOST_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  fi
+else
+  if [ "$(whoami)" == "root" ]; then
+    MESSAGE_HOST_DIR="/etc/opt/chrome/native-messaging-hosts"
+  else
+    MESSAGE_HOST_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+  fi
+fi
 
+cp dk.dtu.openpds.json "${MESSAGE_HOST_DIR}"
+chmod 644 "${MESSAGE_HOST_DIR}/dk.dtu.openpds.json"
+rm -f dk.dtu.openpds.json
 
 ###### initial run of the analysis
 
