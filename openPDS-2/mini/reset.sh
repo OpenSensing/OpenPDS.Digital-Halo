5e63c51df34878ae42656faa377790da607cb8b7
Mieszko Manijak <mieszkomieszko@gmail.com>
2016-03-30 14:21:41 +0200

    cleanup of miniPDS

--- openPDS-2/mini/reset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -f config.db
-rm -f data/*
--- openPDS-2/mini/runner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import subprocess as child 
-from os import path
-
-
-child.call(['./halo.py'])
-#child.call([path.join(this_dir, 'halo.py')])
